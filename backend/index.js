27ea00bad709440baca5b8c454f5f794b1eef025
Oliversoder06 <lexrudolf06@gmail.com>
2024-11-30 04:11:01 +0100

    BUG! NPM RUN DEV NOT WORKING

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from "@apollo/server/standalone"
- 
+
+import mergeResolvers from "./resolvers/index.js"
+import mergeTypeDefs from "./typeDefs/index.js"
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  typeDefs: mergeTypeDefs,
+  resolvers: mergeResolvers,
 })
- 
+
 const { url } = await startStandaloneServer(server)
- 
+
 console.log(`🚀 Server ready at ${url}`)
